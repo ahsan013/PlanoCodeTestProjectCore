974d5c833263a3f6925501f18bd4432e460d032c
Ahsan <ahsan.aftab13@gmail.com>
2020-03-13 13:04:52 +0800

    Plano Test .net core completed

---
+++ PlanoCodeTestProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PlanoCodeTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a number:");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (CheckPalindrome(num))
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+
+        public static bool CheckPalindrome(int num)
+        {
+            string numStr = num.ToString();
+            for (int i = 0; i < numStr.Length - 1; i++)
+            {
+
+                if (numStr[i] == numStr[i + 1])
+                    return true;
+            }
+            return false;
+        }
+
+    }
+    }
+
